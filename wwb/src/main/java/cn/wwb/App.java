536f0ee456f01b4e351bb1e41903bb62da124198
WWB <wenbo_0414@163.com>
2021-04-07 00:58:19 +0800

    1111

---
+++ wwb/src/main/java/cn/wwb/App.java
@@ -0,0 +1,18 @@
+package cn.wwb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
