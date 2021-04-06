bd91d940124315cf8f60e72fe5704727a2001b52
WWB <wenbo_0414@163.com>
2021-04-07 01:03:51 +0800

    9999

--- wwb/src/main/java/cn/wwb/App.java
+++ wwb/src/main/java/cn/wwb/App.java
@@ -13,6 +13,5 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
     }
 }
